ab5b8eea50e2db1420f38d80cbdf96385144799f
bmc1995 <billymccrackin123@gmail.com>
2020-10-21 09:51:22 -0600

    Updates

---
+++ precache-manifest.66588344c87dbd290d50d1db514f8612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6247d274b1d60b1b2593d51786a1373f",
+    "url": "/Flight_tracker/index.html"
+  },
+  {
+    "revision": "2440d9980c8ebb895cf8",
+    "url": "/Flight_tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d563d8e538104f16a2ee",
+    "url": "/Flight_tracker/static/js/2.2b2f537b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Flight_tracker/static/js/2.2b2f537b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2440d9980c8ebb895cf8",
+    "url": "/Flight_tracker/static/js/main.0a33276f.chunk.js"
+  },
+  {
+    "revision": "c351e60a3e7f5ba35b06",
+    "url": "/Flight_tracker/static/js/runtime-main.46be075e.js"
+  }
+]);
